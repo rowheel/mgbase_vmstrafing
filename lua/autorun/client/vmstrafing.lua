ca68c56d95ac28c82eced7a31ece2a1b4e76db9d
Roberta Wheeler <rob3rt.233@gmail.com>
2022-11-14 15:07:35 +0800

    Add editorconfig

--- lua/autorun/client/vmstrafing.lua
+++ lua/autorun/client/vmstrafing.lua
@@ -5,50 +5,50 @@
 -- param: weapon is a reference to the active weapon
 -- returns: true if the active weapon can be tilted, otherwise false
 local function canTilt(weapon)
-  return weapon.Base == "mg_base" and weapon:GetSlot() > 1
+	return weapon.Base == "mg_base" and weapon:GetSlot() > 1
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can lean, otherwise false
 local function canLean(player)
-  return player:KeyDown(IN_ATTACK2) and not player:KeyDown(IN_SPEED)
+	return player:KeyDown(IN_ATTACK2) and not player:KeyDown(IN_SPEED)
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can strafe right, otherwise false
 local function canStrafeRight(player)
-  return player:KeyDown(IN_MOVERIGHT) and not player:KeyDown(IN_MOVELEFT)
+	return player:KeyDown(IN_MOVERIGHT) and not player:KeyDown(IN_MOVELEFT)
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can strafe left, otherwise false
 local function canStrafeLeft(player)
-  return player:KeyDown(IN_MOVELEFT) and not player:KeyDown(IN_MOVERIGHT)
+	return player:KeyDown(IN_MOVELEFT) and not player:KeyDown(IN_MOVERIGHT)
 end
 
 -- brief: Tilts the view model to target angle
 -- param: weapon is a reference to the active weapon
 -- param: targetAngle indicates the target angle to approach
 local function tiltViewModel(weapon, targetAngle)
-  local currentAngle = weapon.ViewModelOffsets.Aim.Angles.roll
-  if math.abs(currentAngle) != math.abs(targetAngle) then
-    local roll = math.ApproachAngle(currentAngle, targetAngle, 1)
-    weapon.ViewModelOffsets.Aim.Angles.roll = roll
-  end
+	local currentAngle = weapon.ViewModelOffsets.Aim.Angles.roll
+	if math.abs(currentAngle) != math.abs(targetAngle) then
+		local roll = math.ApproachAngle(currentAngle, targetAngle, 1)
+		weapon.ViewModelOffsets.Aim.Angles.roll = roll
+	end
 end
 
 -- brief: Registers a hook that tilts the view model when strafing left and right
 -- returns: nil
 hook.Add("Think", "vmstrafing", function()
-  if canTilt(LocalPlayer():GetActiveWeapon()) then
-    if not canLean(LocalPlayer()) then
-      return tiltViewModel(LocalPlayer():GetActiveWeapon(), 0)
-    end
-    if canStrafeRight(LocalPlayer()) then
-      return tiltViewModel(LocalPlayer():GetActiveWeapon(), 45)
-    end
-    if canStrafeLeft(LocalPlayer()) then
-      return tiltViewModel(LocalPlayer():GetActiveWeapon(), -45)
-    end
-  end
+	if canTilt(LocalPlayer():GetActiveWeapon()) then
+		if not canLean(LocalPlayer()) then
+			return tiltViewModel(LocalPlayer():GetActiveWeapon(), 0)
+		end
+		if canStrafeRight(LocalPlayer()) then
+			return tiltViewModel(LocalPlayer():GetActiveWeapon(), 45)
+		end
+		if canStrafeLeft(LocalPlayer()) then
+			return tiltViewModel(LocalPlayer():GetActiveWeapon(), -45)
+		end
+	end
 end)
