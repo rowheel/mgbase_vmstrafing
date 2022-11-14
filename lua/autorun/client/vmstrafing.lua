e4754702f76538c9d61a8e7e5edb21ff8e720f03
Roberta Wheeler <rob3rt.233@gmail.com>
2022-11-14 08:18:24 +0800

    Update vmstrafing.lua

--- lua/autorun/client/vmstrafing.lua
+++ lua/autorun/client/vmstrafing.lua
@@ -5,25 +5,25 @@
 -- param: weapon is a reference to the active weapon
 -- returns: true if the active weapon can be tilted, otherwise false
 local function CanTilt(weapon)
-  return weapon.Base == "mg_base" && weapon:GetSlot() > 1
+  return weapon.Base == "mg_base" and weapon:GetSlot() > 1
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can lean, otherwise false
 local function CanLean(player)
-  return player:KeyDown(IN_ATTACK2) && !player:KeyDown(IN_SPEED)
+  return player:KeyDown(IN_ATTACK2) and not player:KeyDown(IN_SPEED)
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can strafe right, otherwise false
 local function CanStrafeRight(player)
-  return player:KeyDown(IN_MOVERIGHT) && !player:KeyDown(IN_MOVELEFT)
+  return player:KeyDown(IN_MOVERIGHT) and not player:KeyDown(IN_MOVELEFT)
 end
 
 -- param: player is a reference to the local player
 -- returns: true if the local player can strafe left, otherwise false
 local function CanStrafeLeft(player)
-  return player:KeyDown(IN_MOVELEFT) && !player:KeyDown(IN_MOVERIGHT)
+  return player:KeyDown(IN_MOVELEFT) and not player:KeyDown(IN_MOVERIGHT)
 end
 
 -- brief: Tilt the weapon's view model to target angle
@@ -31,7 +31,7 @@ end
 -- param: targetAngle indicates the target angle to approach
 local function TiltViewModel(weapon, targetAngle)
   local currentAngle = weapon.ViewModelOffsets.Aim.Angles.roll
-  if math.abs(currentAngle) != math.abs(targetAngle) then
+  if math.abs(currentAngle) < math.abs(targetAngle) then
     local roll = math.ApproachAngle(currentAngle, targetAngle, 1)
     weapon.ViewModelOffsets.Aim.Angles.roll = roll
   end
@@ -40,7 +40,7 @@ end
 -- brief: Causes the Modern Warfare Base view models to tilt when strafing left and right
 hook.Add("Think", "vmstrafing", function()
   if CanTilt(LocalPlayer():GetActiveWeapon()) then
-    if !CanLean(LocalPlayer()) then
+    if not CanLean(LocalPlayer()) then
       return TiltViewModel(LocalPlayer():GetActiveWeapon(), 0)
     end
     if CanStrafeRight(LocalPlayer()) then
